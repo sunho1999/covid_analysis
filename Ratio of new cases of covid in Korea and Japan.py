45eb11031fc793e7835d1b6a8639889af0d52f4f
sunho1999 <wmfrlek11@naver.com>
2021-11-07 23:19:57 +0900

    한국 일본 날짜별 신규 확진자 비율 시각화

---
+++ Ratio of new cases of covid in Korea and Japan.py
@@ -0,0 +1,34 @@
+import torch
+import pandas as pd
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.set_printoptions(precision=2, suppress=True)  #과학적 표기 없애고 소수 2짜리까지 표현
+
+
+df = pd.read_csv("./owid-covid-data.csv")
+
+df['date'] = pd.to_datetime(df['date'])
+
+date_df = df.set_index(['date'])
+per_df = date_df.loc[:,['location','total_cases_per_million','new_cases_per_million','people_vaccinated_per_hundred','people_fully_vaccinated_per_hundred']]
+
+jp_df = per_df[per_df['location'] == 'Japan']
+kr_df = per_df[per_df['location'] == 'South Korea']
+
+sr_one = jp_df['new_cases_per_million'] # new_cases_per_million
+sr_two = kr_df['new_cases_per_million'] # new_cases_per_million
+
+fig = plt.figure(figsize = (20,5))
+ax = fig.add_subplot(1,1,1)
+
+ax.plot(sr_one.index,sr_one.values,marker='o',markerfacecolor = 'green',markersize = 1, color = 'olive',linewidth =0.5, label = "Ratio of covid in japan")
+ax.plot(sr_two.index,sr_two.values,marker='o',markerfacecolor = 'red',markersize = 1, color = 'magenta',linewidth =0.5, label = "Ratio of covid in Korea")
+
+ax.legend(loc = 'best')
+ax.set_title("Ratio of new cases of covid in Korea and Japan")
+ax.set_xlabel("date")
+ax.set_ylabel("Percentage of new cases by date")
+
+plt.show()
